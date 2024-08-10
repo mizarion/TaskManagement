6d29e352cc54ace59b4b17d6d146f8e6a0b14203
mizarion <zsgluschenko@edu.hse.ru>
2024-08-10 20:56:00 +0900

    moved to postgres in docker compose

--- src/test/java/com/mizarion/taskmanagement/AbstractTaskManagementApplicationTests.java
+++ src/test/java/com/mizarion/taskmanagement/AbstractTaskManagementApplicationTests.java
@@ -15,7 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public abstract class AbstractTaskManagementApplicationTests {
